1e7e5dda7c1881eedc5ef5996392c26be33c9cad
Javibuh <javigarciafortis@gmail.com>
2025-03-25 13:36:12 +0100

    VolunteerController actualizado (unused imports borrados)

--- Backend/src/main/java/solidarityhub/backend/controller/VolunteerController.java
+++ Backend/src/main/java/solidarityhub/backend/controller/VolunteerController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import solidarityhub.backend.model.Volunteer;
 import solidarityhub.backend.service.VolunteerService;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/volunteers")
