97e35e5221b5fd5746c09bc57c970ea9294435ca
Hugo Juan Gomez <hugojuan09082004@gmail.com>
2025-05-19 11:37:20 +0200

    Traducidas vistas de almacenes

--- Frontend/src/main/java/solidarityhub/frontend/views/resources/storage/StorageView.java
+++ Frontend/src/main/java/solidarityhub/frontend/views/resources/storage/StorageView.java
@@ -1,19 +1,23 @@
 package solidarityhub.frontend.views.resources.storage;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.server.VaadinSession;
 import solidarityhub.frontend.dto.CatastropheDTO;
 import solidarityhub.frontend.dto.StorageDTO;
+import solidarityhub.frontend.i18n.Translator;
 import solidarityhub.frontend.service.CatastropheService;
 import solidarityhub.frontend.service.StorageService;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class StorageView extends VerticalLayout {
     private final StorageService storageService;
@@ -27,6 +31,8 @@ public class StorageView extends VerticalLayout {
     private Grid.Column<StorageDTO> nameColumn;
     private Grid.Column<StorageDTO> statusColumn;
 
+    private final Translator translator;
+
     public StorageView(CatastropheDTO catastrophe) {
         this.storageService = new StorageService();
         this.catastropheService = new CatastropheService();
@@ -34,6 +40,16 @@ public class StorageView extends VerticalLayout {
         this.selectedCatastrophe = catastrophe;
 
         this.storageGrid = new Grid<>(StorageDTO.class, false);
+
+        Locale sessionLocale = VaadinSession.getCurrent().getAttribute(Locale.class);
+        if (sessionLocale != null) {
+            UI.getCurrent().setLocale(sessionLocale);
+        }else{
+            VaadinSession.getCurrent().setAttribute(Locale.class, new Locale("es"));
+            UI.getCurrent().setLocale(new Locale("es"));
+        }
+        translator = new Translator(UI.getCurrent().getLocale());
+
         buildView();
     }
 
@@ -62,7 +78,7 @@ public class StorageView extends VerticalLayout {
 
         if (storageDataProvider.getItems().isEmpty()) {
             storageGrid.setVisible(false);
-            add(new Span("No hay almacenes disponibles para esta catástrofe."));
+            add(new Span(translator.get("no_storages")));
         } else {
             storageGrid.setVisible(true);
             storageGrid.setDataProvider(storageDataProvider);
@@ -77,16 +93,16 @@ public class StorageView extends VerticalLayout {
     //===============================Get Components=========================================
     private void getGridColumns() {
         nameColumn = storageGrid.addColumn(StorageDTO::getName)
-                .setHeader("Nombre")
+                .setHeader(translator.get("storage_name"))
                 .setAutoWidth(true);
 
         statusColumn = storageGrid.addColumn(this::getStorageStatusText)
-                .setHeader("Estado")
+                .setHeader(translator.get("storage_status"))
                 .setAutoWidth(true);
     }
 
     private String getStorageStatusText(StorageDTO storage) {
-        return storage.isFull() ? "Almacén lleno" : "Almacén disponible";
+        return storage.isFull() ? translator.get("storage_full") : translator.get("storage_available");
     }
 
     private Component getGridFilterHeader(String headerText, Component filterComponent) {
